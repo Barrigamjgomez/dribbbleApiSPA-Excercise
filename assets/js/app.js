e618b2cb88953e3d095389f31fba5511c669fc70
Mariajose <tienditacote@gmail.com>
2018-02-21 14:58:08 -0300

    bla

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,7 @@
  var provider = new firebase.auth.GoogleAuthProvider();
 
 
- function signIn(){
+ function user(){
  firebase.auth().signInWithPopup(provider).then(function(result) {
   // This gives you a Google Access Token. You can use it to access the Google API.
   var token = result.credential.accessToken;
